587607f377b4ab60687cf67c05138183d5a17b94
Mete <25986959+mtoprak53@users.noreply.github.com>
2021-06-15 11:32:41 -0400

    06/15/21

--- u2900/app.py
+++ u2900/app.py
@@ -1,14 +1,18 @@
 import os
+import requests
 
 from flask import Flask, redirect, render_template, request, flash, session, g, abort
 from flask_debugtoolbar import DebugToolbarExtension
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError   # for already taken usernames
 
-# from forms import SomeForm   #  <<== ENTER FORMS
-from models import db, connect_db, Food
-from secrets import API_SECRET_KEY
+from forms import UserAddForm, LoginForm
+from models import db, connect_db, Food, FoodLog, User, UserInfo
+
+from hidden import API_KEY
 
 CURR_USER_KEY = "curr_user"
+
+BASE_URL = "https://www.foodrepo.org/api/v3"
 
 app = Flask(__name__)
 
@@ -19,14 +23,122 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", API_SECRET_KEY)
+# app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", API_SECRET_KEY)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "top_secret")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
+# db.drop_all()
+# db.create_all()
+
+
+####################################################################################
+# User signup/login/logout
+
+
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr_user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+    else:
+        g.user = None
+    
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    """Handle user signup.
+
+    Create new user and add to DB. Redirect to home page.
+
+    If form not valid, present form.
+
+    If  there already is a user with that username: flash message and re-present the form.
+    """
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+    form = UserAddForm()
+
+    if form.validate_on_submit():   # .validate_on_submit()
+        try:
+            user = User.signup(   # .signup()
+                username=form.username.data,
+                password=form.password.data
+                # first_name=form.first_name.data,
+                # last_name=form.last_name.data,
+            )
+            db.session.commit()
+            user_info = UserInfo(
+                user_id=user.id,
+                calorie_need=form.calorie_need.data,
+                calorie_limit=form.calorie_limit.data                
+            )
+            db.session.add(user_info)
+            db.session.commit()
+
+        except IntegrityError as e:
+            flash("Username already taken", 'danger')
+            return render_template('signup.html', form=form)
+        
+        do_login(user)   # do_login()
+
+        return redirect('/')
+    
+    else:
+        return render_template('signup.html', form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Handle user login."""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():   # .validate_on_submit()
+        user = User.authenticate(   # .authenticate()
+            form.username.data,
+            form.password.data
+        )
+
+        if user:
+            do_login(user)   # do_login()
+            flash(f"Hello, {user.username}!", 'success')
+            return redirect('/')
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """Handle logout of user."""
+
+    do_logout()   # do_logout
+
+    flash("You have successfully logged out.", 'success')
+    return redirect("/login")
+
 
 ####################################################################################
 # 
+
 
 @app.route('/add-food/<food_name>', methods=["POST"])
 def add_food(food_name):
@@ -41,6 +153,62 @@ def add_food(food_name):
     return f"{food_name} added!"
 
 
+@app.route('/food-search', methods=["GET", "POST"])
+def food_search():
+    """
+    """    
+
+    if request.form:
+        food = request.form["food"]
+
+        print("#"*30)
+        print(food)
+        print("#"*30)
+
+        ENDPOINT = '/products'
+        url = BASE_URL + ENDPOINT
+
+        print("#"*30)
+        print(url)
+        print("#"*30)
+
+        query = {
+            "query": {
+                "wildcard": {
+                    "_all_names" : f"*{food}*"
+                }
+            }
+        }
+
+        headers = {
+            'Authorization': 'Token token=' + API_KEY,
+            'Accept': 'application/json',
+            'Content-Type': 'application/vnd.api+json',
+            'Accept-Encoding': 'gzip,deflate'
+        }
+
+        r = requests.post(url, json=query, headers=headers)
+        if r.status_code == 200:
+            results = r.json()
+            return render_template('results.html', results=results)
+        else:
+            # return render_template('results.html', status_code=r.status_code)
+
+            print("#"*30)
+            print(r)
+            print("#"*30)
+
+            return render_template('results.html', r=r)
+
+
+
+        # return f"Searched food is {food}"
+        return render_template('results.html', results=results)
+
+    return render_template('search.html')
+
+
+
 ####################################################################################
 # 
 
@@ -51,15 +219,22 @@ def add_food(food_name):
 
 @app.route('/')
 def homepage():
-    """
-    """
+    """Show homepage"""
 
-    # if g.user:
-    if False:
-        pass
+    if g.user:
+        print("#"*30)
+        print(f"g.user => {g.user}")
+        print("#"*30)
+
+        # user = User.query.get(g.user)
+        # print("#"*30)
+        # print(f"user => {user}")
+        # print("#"*30)
+
+        # user = g.user
+        return render_template('home.html', user=g.user)
     else:
-        # return render_template('home-anon.html')   # ORIGINAL
-        return render_template('home.html')   # TEST
+        return render_template('home-anon.html')
 
 
 @app.errorhandler(404)
--- u2900/models.py
+++ u2900/models.py
@@ -1,6 +1,6 @@
 """SQLAlchemy models for Calorie Counter"""
 
-from datetime import datetime
+from datetime import datetime, date
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -12,21 +12,23 @@ bcrypt = Bcrypt()
 
 
 class FoodLog(db.Model):
-    """
-    """
+    """Each food consumption entry."""
 
-    __tablename___ = 'foodlogs'
+    __tablename__ = 'foodlogs'
+
+    id = db.Column(
+        db.Integer,
+        primary_key = True
+    )
 
     user_id = db.Column(
         db.Integer,
-        db.ForeignKey('users.id', ondelete="cascade"),
-        primary_key=True
+        db.ForeignKey('users.id', ondelete="cascade")
     )
 
     food_id = db.Column(
         db.Integer,
-        db.ForeignKey('foods.id', ondelete="cascade"),
-        primary_key=True
+        db.ForeignKey('foods.id', ondelete="cascade")
     )
 
     amount = db.Column(
@@ -37,13 +39,16 @@ class FoodLog(db.Model):
     date = db.Column(
         db.DateTime,
         nullable=False,
-        default=datetime.utcnow
+        default=date.today()
     )
+
+    user = db.relationship('User')
+
+    food = db.relationship('Food')
 
 
 class Food(db.Model):
-    """
-    """
+    """Info of the foods."""
 
     __tablename__ = 'foods'
 
@@ -61,13 +66,36 @@ class Food(db.Model):
 
     unit_calories = db.Column(
         db.Integer,
-        nullable=False`
+        nullable=False
     )
 
 
+class UserInfo(db.Model):
+    """User details."""
+
+    __tablename__ = 'userinfos'
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete='cascade'),
+        primary_key=True
+    )
+
+    calorie_limit = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    calorie_need = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    user = db.relationship('User')
+
+
 class User(db.Model):
-    """
-    """
+    """User credentials."""
 
     __tablename__ = 'users'
 
@@ -88,12 +116,24 @@ class User(db.Model):
         nullable=False
     )
 
+
+
+
+    ###   MODIFY THIS   ###
+    # def __repr__(self):
+    #     """Show info about the user."""
+        
+    #     return f"<User #{self.id}: {self.username}, {self.password}>"
+
+
     @classmethod
     def signup(cls, username, password):
-        """Signup user.
+        """
+        Signup user.
         Hashes password and adds user to the system.
         """
 
+        # .generate_password_hash()
         hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
 
         user = User(
@@ -104,6 +144,29 @@ class User(db.Model):
         db.session.add(user)
         return user
 
+    @classmethod
+    def authenticate(cls, username, password):
+        """
+        Find user with `username` & `password`.
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password,
+        and if it finds such a user, returns that user object.
+        If it cannot find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            # .check_password_hash()
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+
+
+
+    
 
 ###########################################################
 # DATABASE CONNECTION:
